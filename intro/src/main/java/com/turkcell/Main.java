c9fac6539061861806b8d10b27ac420ca0c60dec
Orhan Gölcür <orhangolcur0@gmail.com>
2026-04-07 11:04:15 +0300

    primitive vs reference types

--- intro/src/main/java/com/turkcell/Main.java
+++ intro/src/main/java/com/turkcell/Main.java
@@ -1,7 +1,60 @@
 package com.turkcell;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Turkcell!");
+        // Primitive types
+        int a = 0;
+        int b = a;
+        a = 10;
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("------------------");
+
+        // Reference types
+        int[] c = {0, 1, 2, 3};
+        int[] d = c;
+        d[3] = 30;
+        System.out.println(c[3]);
+        System.out.println(d[3]);
+
+        System.out.println("------------------");
+        
+        System.out.println(a == b);
+        System.out.println(c == d);
+
+        System.out.println("------------------");
+
+        int[] x = {0, 1, 2, 3};
+        int[] y = {0, 1, 2, 3};
+        System.out.println(x);
+        System.out.println(x == y);
+        System.out.println(Arrays.equals(x, y));
+
+        System.out.println("------------------");
+
+        String s1 = "Merhaba";
+        String s2 = "Merhaba";
+
+        System.out.println(s1 == s2); // String pool
+        System.out.println(s1.equals(s2)); // Daha güvenli
+
+        System.out.println("------------------");
+
+        String s3 = "Turkcell";
+        String s4 = s3.intern();
+        // intern() metodu, string'i string pool'a ekler ve referansını döner
+
+        System.out.println(s3 == s4);
+
+        System.out.println("------------------");
+
+        String s5 = "Turkcell";
+        String s6 = new String("Turkcell"); // yeni bir String nesnesi oluşturur, string pool'dan değil heap'ten referans alır
+
+        System.out.println(s5 == s6);
+
     }
 }
