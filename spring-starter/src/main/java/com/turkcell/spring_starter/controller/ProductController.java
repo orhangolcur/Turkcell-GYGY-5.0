ca64e90481a22b12658e16b97e9e1753f6549460
Orhan Gölcür <orhangolcur0@gmail.com>
2026-04-10 20:10:35 +0300

    .

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -1,9 +1,6 @@
 package com.turkcell.spring_starter.controller;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.turkcell.spring_starter.dto.ProductCreatedResponse;
 import com.turkcell.spring_starter.dto.ProductForCreateDto;
 import com.turkcell.spring_starter.model.Product;
---
+++ spring-starter/src/main/java/com/turkcell/spring_starter/service/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package com.turkcell.spring_starter.service;
+
+// Implementation => Business Logic'in yazıldığı sınıf
+public class ProductServiceImpl {
+    // Controller'ın aktracağı işleri tanımlarız.
+    // İş kodu yazılır.
+}
