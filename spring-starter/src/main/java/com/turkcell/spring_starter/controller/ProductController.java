148fccf956b7480785f2032fa6be183b010d3909
Orhan Gölcür <orhangolcur0@gmail.com>
2026-04-10 19:00:00 +0300

    temel controller methodları

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -1,31 +1,48 @@
 package com.turkcell.spring_starter.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.turkcell.spring_starter.model.Product;
 
-@RestController // Bu class bir rest contollerdır, içini uygulama başladığında tara, http=> function tanımlarını al
-@RequestMapping("/api/product") // http://localhost:8080/api/product => sayHi() çalışır
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
 
-    @GetMapping("") // http://localhost:8080/api/product => sayHi() çalışır
-    public String sayHi(String name, int age) {
-        return "Hi, " + name + " yaşınız: " + age;
+    // In-Memory 
+    private List<Product> productList = new ArrayList<>();
+
+    @GetMapping()
+    public List<Product> getAll() {
+        return productList;
     }
 
-    // http://localhost:8080/api/product/hello => sayHello() çalışır 
-    @GetMapping("hello/{name}/{age}") // http://localhost:8080/api/product/hello/John/30 => sayHello() çalışır, name=John, age=30 olur
-    public String sayHello(@PathVariable String name, @PathVariable int age) {
-        return "Hello, " + name + " yaşınız: " + age;
-    } 
-
-    @PostMapping // http://localhost:8080/api/product => add() çalışır, body kısmına json formatında product bilgisi gönderilir
-    public String add(@RequestBody Product product) { // Json => Java objesine
-        return product.getId() + " ID'li ürün başarıyla eklendi, fiyatı: " + product.getPrice();
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id) {
+        return productList.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElse(null);
     }
+
+    @PostMapping()
+    public void createProduct(@RequestBody Product product) {
+        productList.add(product);
+    }
+
+    @PutMapping
+    public void updateProduct(@RequestBody Product product) {}
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable int id) {}
+    
 }
