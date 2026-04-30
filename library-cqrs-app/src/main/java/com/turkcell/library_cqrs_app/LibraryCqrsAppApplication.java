8b715ed8679aeda38d4387288d2e60ad1e05ad42
Orhan Gölcür <orhangolcur0@gmail.com>
2026-04-30 22:27:18 +0300

    library cqrs app eklendi

---
+++ library-cqrs-app/src/main/java/com/turkcell/library_cqrs_app/LibraryCqrsAppApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.library_cqrs_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCqrsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCqrsAppApplication.class, args);
+	}
+
+}
