6b29a039d49fda93f96ee474b38bed25f8e9dfcf
Orhan Gölcür <orhangolcur0@gmail.com>
2026-05-04 20:12:31 +0300

    mapper

--- spring-cqrs/src/main/java/com/turkcell/spring_cqrs/application/features/category/command/create/CreateCategoryCommandHandler.java
+++ spring-cqrs/src/main/java/com/turkcell/spring_cqrs/application/features/category/command/create/CreateCategoryCommandHandler.java
@@ -1,6 +1,7 @@
 package com.turkcell.spring_cqrs.application.features.category.command.create;
 
 import org.springframework.stereotype.Component;
+import com.turkcell.spring_cqrs.application.features.category.mapper.CategoryMapper;
 import com.turkcell.spring_cqrs.application.features.category.rule.CategoryBusinessRules;
 import com.turkcell.spring_cqrs.core.mediator.cqrs.CommandHandler;
 import com.turkcell.spring_cqrs.domain.Category;
@@ -9,26 +10,25 @@ import com.turkcell.spring_cqrs.persistence.repository.CategoryRepository;
 @Component // Spring tarafından yönetilen bir bean olduğunu belirtir
 public class CreateCategoryCommandHandler implements CommandHandler<CreateCategoryCommand, CreatedCategoryResponse>
 {
-
     private final CategoryRepository categoryRepository;
     private final CategoryBusinessRules categoryBusinessRules;
-    
-    public CreateCategoryCommandHandler(CategoryRepository categoryRepository, CategoryBusinessRules categoryBusinessRules) {
+    private final CategoryMapper categoryMapper;
+
+    public CreateCategoryCommandHandler(CategoryRepository categoryRepository, CategoryBusinessRules categoryBusinessRules, CategoryMapper categoryMapper) {
         this.categoryRepository = categoryRepository;
         this.categoryBusinessRules = categoryBusinessRules;
+        this.categoryMapper = categoryMapper;
     }
 
     @Override
     public CreatedCategoryResponse handle(CreateCategoryCommand command) 
     {
-        categoryBusinessRules.categoryWithSameNameMustNotExist(command.name());
+        categoryBusinessRules.categoryWithSameNameMustNotExist(command.name()); // rule
 
-        Category category = new Category();
-        category.setName(command.name());
+        Category category = categoryMapper.categoryFromCreateCommand(command); // mapping
 
         categoryRepository.save(category);
 
-        CreatedCategoryResponse response = new CreatedCategoryResponse(category.getId(), category.getName());
-        return response;
+        return categoryMapper.createdCategoryResponseFromCategory(category); // mapping
     }
 }
